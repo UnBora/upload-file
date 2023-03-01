00e1fb8224b6ecae9ae4bc0cfbed1957b49bf77f
UnBora <bora.un2000@gmail.com>
2023-03-01 10:26:42 +0700

    Upload file rest

--- src/main/java/com/example/uploadfile002/controller/FileController.java
+++ src/main/java/com/example/uploadfile002/controller/FileController.java
@@ -6,16 +6,13 @@ import com.example.uploadfile002.service.DBFileStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
